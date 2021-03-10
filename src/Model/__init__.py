093896934267ccf1cd08ee9c3f5d025e36c671da
Marco Hirth <hirth.marco@gmx.de>
2021-03-10 20:03:08 +0100

    file system manip for unix

--- src/Model/__init__.py
+++ src/Model/__init__.py
@@ -1,11 +1,13 @@
 import os
 import configparser
 import json
+import platform
 from time import sleep
 from pubsub import pub
 from datetime import datetime, timedelta
-from win32file import CreateFile, SetFileTime, GetFileTime, CloseHandle
-from win32file import GENERIC_WRITE, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, FILE_SHARE_WRITE
+if platform.system() == "Windows":
+    from win32file import CreateFile, SetFileTime, GetFileTime, CloseHandle
+    from win32file import GENERIC_WRITE, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, FILE_SHARE_WRITE
 
 from Model.FirefoxModel import FirefoxModel
 from Model.EdgeModel import EdgeModel
@@ -197,9 +199,11 @@ class Model:
         delta = start_timestamp - end_timestamp
 
         if config.current_os == "Windows":
-
+            # Define function to modify filetimes on Windows
             def setTime(path, delta):
-                fh = CreateFile(path, GENERIC_WRITE, FILE_SHARE_WRITE, None, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, 0)
+                fh = CreateFile(path, GENERIC_WRITE, 
+                                FILE_SHARE_WRITE, None, 
+                                OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, 0)
                 cTime, aTime, mTime = GetFileTime(fh)
                 cTime = datetime.fromtimestamp(cTime.timestamp() - delta)
                 aTime = datetime.fromtimestamp(aTime.timestamp() - delta)
@@ -207,13 +211,16 @@ class Model:
                 SetFileTime(fh, cTime, aTime, mTime)
                 CloseHandle(fh)
         else:
-
+            # Define function to modify filetimes on Linux/Mac
             def setTime(path, delta):
                 a_time = os.path.getatime(path)
                 m_time = os.path.getmtime(path)
-                print(a_time, m_time)
+                a_time = a_time - delta
+                m_time = m_time - delta
+                os.utime(path, (a_time, m_time))
         
         for path in paths:
+                # recursivly iterate through the dirs and files and change time
                 for root, dir, files in os.walk(path):
                     for d in dir:
                         path = os.path.join(root, d)
@@ -221,7 +228,9 @@ class Model:
                             setTime(path, delta)
                         except:
                             pub.sendMessage("logging",
-                                            message="Datei " + path + " konnten nicht editiert werden!", 
+                                            message="Datei " + 
+                                            path + 
+                                            " konnten nicht editiert werden!", 
                                             lvl="info")
                     for f in files:
                         path = os.path.join(root, f)
@@ -229,7 +238,9 @@ class Model:
                             setTime(path, delta)
                         except:
                             pub.sendMessage("logging",
-                                            message="Datei " + path + " konnten nicht editiert werden!", 
+                                            message="Datei " + 
+                                            path + 
+                                            " konnten nicht editiert werden!", 
                                             lvl="info")
 
         self.browsermodel.get_data()
